de7d23a1a1ccae4200118a4e1f7e4cfa381dae42
ChrisStricker <skaven13@sbcglobal.net>
2015-11-03 21:16:28 -0600

    updated with google place details api and jsonp

--- js/app.js
+++ js/app.js
@@ -3,33 +3,40 @@ var MAP_CENTER = {lat:38.6272222, lng:-90.1977778};
 
 // Global Variables
 var sites = [
-	{name:"Gateway Arch", latLng:{lat:38.624674, lng:-90.184941}},
-	{name:"Busch Stadium", latLng:{lat:38.622644, lng:-90.192812}},
-	{name:"Missouri Botanical Garden", latLng:{lat:38.612008, lng:-90.260196}},
-	{name:"St. Louis Zoo", latLng:{lat:38.63578, lng:-90.288734}},
-	{name:"Fabulous Fox Theater", latLng:{lat:38.639003, lng:-90.232133}}
+  {name:"Gateway Arch", latLng:{lat:38.6246910, lng:-90.1847763}, id:"ChIJteVBdWDwwIcRNKiWxoko7Xk"},
+  {name:"Busch Stadium", latLng:{lat:38.6226188, lng:-90.1928209}, id:"ChIJF88LRBuz2IcRJ59Q49dieaU"},
+  {name:"Missouri Botanical Garden", latLng:{lat:38.6127672, lng:-90.2593798}, id:"ChIJgc3Sqf-02IcRgFDe_IDFsGA"},
+  {name:"Saint Louis Zoo", latLng:{lat:38.6350068, lng:-90.2907165}, id:"ChIJLQYEzFy12IcRMOjolwEIdWA"},
+  {name:"Fabulous Fox Theater", latLng:{lat:38.639003, lng:-90.232133}, id:"ChIJdVbZ8KS02IcRvTYPK1y3gFU"}
 ];
+
+var mapDiv = document.getElementById("map-content");
+
+var contentString = '<div id="content"></div>';
 
 var ViewModel = function() {
   var self = this;
   
-  self.googleMap = new google.maps.Map(document.getElementById('map'), {
+  self.googleMap = new google.maps.Map(mapDiv, {
     center: MAP_CENTER,
     zoom: 13
   });
   
   self.allPlaces = [];
-  locationData.forEach(function(place) {
+  sites.forEach(function(place) {
     self.allPlaces.push(new Place(place));
   });
   
   self.allPlaces.forEach(function(place){
     var markerOptions = {
       map: self.googleMap,
-      position: place.latLng
+      position: place.latLng,
+      placeId: place.id,
+      animation: google.maps.Animation.DROP
     };
     
     place.marker = new google.maps.Marker(markerOptions);
+    place.marker.addListener('click', activateLocation);
     // You might also add listeners onto the marker, such as "click" listeners.
   });
   
@@ -59,7 +66,25 @@ var ViewModel = function() {
   function Place(dataObj) {
     this.locationName = dataObj.name;
     this.latLng = dataObj.latLng;
+    this.id = dataObj.id;
     this.marker = null;
+  }
+  
+  function activateLocation() {  
+    // Clear any animated markers and toggle the clicked marker's animation
+    self.allPlaces.forEach(function(place){
+      place.marker.setAnimation(null);
+    });
+	  
+    this.setAnimation(google.maps.Animation.BOUNCE);
+	  
+    // Open the info window
+    $.getJSON( "https://maps.googleapis.com/maps/api/place/details/json?key=AIzaSyBN6fQCYN1L14ie8uTENpNZCCe9BPxPRbc&placeid=" + this.placeId + "&callback=?", function( data ) {
+      var items = [];
+      $.each( data, function( key, val ) {
+        items.push( "<li id='" + key + "'>" + val + "</li>" );
+      });
+    });
   }
 };
 
