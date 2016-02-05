88db013e5310dafee44ab7fcebe2608a0373a315
ChrisStricker <skaven13@sbcglobal.net>
2016-02-05 15:53:33 -0600

    added event handler for list clicks

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ var FS_CLIENT_ID = "PIP03QKRLW4OYD4WN2KYU3D2E3B5MG44OUEEN4L2QBNFSZXU";
 var FS_CLIENT_SECRET = "GA1PBLEZQFEIWFF5GYXLY0IJC1DBLJSBRAUPBDOP2JTQIY3L";
 var FS_VERSION = "20160131";
 
-// Global Variables
+// Global Variables - id is the google places id, venue is the id for foursquare
 var sites = [
   {name:"Gateway Arch", latLng:{lat:38.6246910, lng:-90.1847763}, id:"ChIJteVBdWDwwIcRNKiWxoko7Xk", venue:"4acbc3fbf964a52013c620e3"},
   {name:"Busch Stadium", latLng:{lat:38.6226188, lng:-90.1928209}, id:"ChIJF88LRBuz2IcRJ59Q49dieaU", venue:"4b155059f964a5206ab023e3"},
@@ -14,106 +14,120 @@ var sites = [
 ];
 
 var mapDiv = document.getElementById("map-content");
-
-//var contentString = '<div id="content"><h3>name</h3><span>address</span><br><span>phone</span><br><a href="url">url</a><p>description</p></div>';
 var contentString = '<div id="content"><div class="row"><h3>name</h3></div><div class="row"><div class="col-xs-4"><img src="fssrc" alt="Foursquare provided image"></div><div class="col-xs-8"><span>address</span><br><span>phone</span><br><a href="fsurl">fsurl</a></div></div><div class="row"><p>description</p></div><div class="row"><span>Rating: fsrating / 10</span></div></div>';
+
 var ViewModel = function() {
-  var self = this;
+	var self = this;
   
-  self.googleMap = new google.maps.Map(mapDiv, {
-    center: MAP_CENTER,
-    zoom: 13
-  });
+	// Define the map
+	self.googleMap = new google.maps.Map(mapDiv, {
+		center: MAP_CENTER,
+		zoom: 13
+	});
   
-  self.infowindow = new google.maps.InfoWindow();
-  self.googlePlaces = new google.maps.places.PlacesService(self.googleMap);
-
-  self.allPlaces = [];
-  sites.forEach(function(place) {
-    self.allPlaces.push(new Place(place));
-  });
+	self.infowindow = new google.maps.InfoWindow();
   
-  self.allPlaces.forEach(function(place){
-    var markerOptions = {
-      map: self.googleMap,
-      position: place.latLng,
-      placeId: place.id,
-      venue: place.venue,
-      animation: google.maps.Animation.DROP
-    };
+	// Location menu support
+	self.shouldShowMenu = ko.observable(true);
+	self.toggleMenu = function() {
+		self.shouldShowMenu(!self.shouldShowMenu());
+	};
+	
+	self.clickMarker = function() {
+		google.maps.event.trigger(this.marker, 'click');
+	};
+	
+	// build place objects
+	self.allPlaces = [];
+	sites.forEach(function(place) {
+		self.allPlaces.push(new Place(place));
+	});
+  
+	self.allPlaces.forEach(function(place){
+		var markerOptions = {
+				map: self.googleMap,
+				position: place.latLng,
+				placeId: place.id,
+				venue: place.venue,
+				animation: google.maps.Animation.DROP
+		};
     
-    place.marker = new google.maps.Marker(markerOptions);
-    place.marker.addListener('click', activateLocation);
-    // You might also add listeners onto the marker, such as "click" listeners.
-  });
+		place.marker = new google.maps.Marker(markerOptions);
+		place.marker.addListener('click', activateLocation);
+	});
   
-  self.visiblePlaces = ko.observableArray();
+	// Support for filtering menu items and markers
+	self.visiblePlaces = ko.observableArray();
   
-  self.allPlaces.forEach(function(place){
-    self.visiblePlaces.push(place);
-  });
+	self.allPlaces.forEach(function(place){
+		self.visiblePlaces.push(place);
+	});
   
-  self.userInput = ko.observable('');
+	self.userInput = ko.observable('');
   
-  self.filterMarkers = function() {
-    var searchInput = self.userInput().toLowerCase();
-    self.visiblePlaces.removeAll();
+	self.filterMarkers = function() {
+		var searchInput = self.userInput().toLowerCase();
+		self.visiblePlaces.removeAll();
     
-    self.allPlaces.forEach(function(place) {
-      place.marker.setVisible(false);
-      if (place.locationName.toLowerCase().indexOf(searchInput) !== -1) {
-        self.visiblePlaces.push(place);
-      }
-    });
-    self.visiblePlaces().forEach(function(place){
-      place.marker.setVisible(true);
-    });
-  };
+		self.allPlaces.forEach(function(place) {
+			place.marker.setVisible(false);
+			if (place.locationName.toLowerCase().indexOf(searchInput) !== -1) {
+				self.visiblePlaces.push(place);
+			}
+		});
+		self.visiblePlaces().forEach(function(place){
+			place.marker.setVisible(true);
+		});
+	};
   
-  function Place(dataObj) {
-    this.locationName = dataObj.name;
-    this.latLng = dataObj.latLng;
-    this.id = dataObj.id;
-    this.venue = dataObj.venue;
-    this.marker = null;
-  }
+	// Definition of a place object
+	function Place(dataObj) {
+		this.locationName = dataObj.name;
+		this.latLng = dataObj.latLng;
+		this.id = dataObj.id;
+		this.venue = dataObj.venue;
+		this.marker = null;
+	}
   
-  function activateLocation() {  
-    var currentMarker = this;
-    // Clear any animated markers and toggle the clicked marker's animation
-    self.allPlaces.forEach(function(place){
-      place.marker.setAnimation(null);
-    });
+	// Function called when a marker is clicked.
+	function activateLocation() {  
+		var currentMarker = this;
+		
+		// Clear any animated markers and toggle the clicked marker's animation
+		self.allPlaces.forEach(function(place){
+			place.marker.setAnimation(null);
+		});
 	  
-    this.setAnimation(google.maps.Animation.BOUNCE);
+		this.setAnimation(google.maps.Animation.BOUNCE);
 
-    //make ajax call to four square for additional information
-    //https://api.foursquare.com/v2/venues/4acbc3fbf964a52013c620e3
-    var fsInfo = $.ajax({
-      url: 'https://api.foursquare.com/v2/venues/' + this.venue,
-      dataType: 'json',
-      data: 'client_id=' + FS_CLIENT_ID + '&client_secret='+ FS_CLIENT_SECRET + '&v=' + FS_VERSION,
-      success: function(data) {
-	var venue = data.response.venue;
-	var content = contentString;
-  	content = content.replace("fssrc", venue.bestPhoto.prefix + "125x125" + venue.bestPhoto.suffix);
-	content = content.replace("name", venue.name);
-  	content = content.replace("address", venue.location.formattedAddress.join("<br/>"));
-  	content = content.replace("phone", venue.contact.formattedPhone);
-	content = content.replace("description", venue.description);
-	content = content.replace("fsrating", venue.rating);
-	content = content.replace(/fsurl/g, venue.url);
-	self.infowindow.setContent(content);
-      },
-      error: function(jqXHR, textStatus, errorThrown) {
-        self.infowindow.setContent('Foursquare information is not available.')		
-      },
-      complete: function(jqXHR, textStatus) {
-      	self.infowindow.open(self.googleMap, currentMarker);
-      }
-    });
-
-  }
+		//make ajax call to Foursquare for additional information
+		var fsInfo = $.ajax({
+			url: 'https://api.foursquare.com/v2/venues/' + this.venue,
+			dataType: 'json',
+			data: 'client_id=' + FS_CLIENT_ID + '&client_secret='+ FS_CLIENT_SECRET + '&v=' + FS_VERSION,
+			success: function(data) {
+				//Build the resulting string and add that to the infoWindow
+				var venue = data.response.venue;
+				var content = contentString;
+				content = content.replace("fssrc", venue.bestPhoto.prefix + "125x125" + venue.bestPhoto.suffix);
+				content = content.replace("name", venue.name);
+				content = content.replace("address", venue.location.formattedAddress.join("<br/>"));
+				content = content.replace("phone", venue.contact.formattedPhone);
+				content = content.replace("description", venue.description);
+				content = content.replace("fsrating", venue.rating);
+				content = content.replace(/fsurl/g, venue.url);
+				self.infowindow.setContent(content);
+			},
+			error: function(jqXHR, textStatus, errorThrown) {
+				//If an error is detected, produce a generic message.
+				self.infowindow.setContent('Foursquare information is not available.');		
+			},
+			complete: function(jqXHR, textStatus) {
+				//Open the infoWindow
+				self.infowindow.open(self.googleMap, currentMarker);
+			}
+		});
+	}
 };
 
 ko.applyBindings(new ViewModel());
